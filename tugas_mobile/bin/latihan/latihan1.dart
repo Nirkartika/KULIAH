6871fde03840db5aefc64257c073e54a86eed560
DeaRevananda <dearevanandaa04@gmail.com>
2025-09-03 23:25:54 +0900

    update tugas latihan

---
+++ tugas_mobile/bin/latihan/latihan1.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nama = 'Nirkartika Kamal';
+  int umur = 21;
+  double tinggi = 148.0;
+  bool statusMahasiswaAktif = true;
+
+  print('Nama   : $nama');
+  print('Umur   : $umur');
+  print('Tinggi : $tinggi');
+  print('Status : ${statusMahasiswaAktif ? 'Aktif' : 'Tidak Aktif'}');
+}
---
+++ tugas_mobile/bin/latihan/latihan3.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Masukkan panjang: ');
+  double panjang = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Masukkan lebar: ');
+  double lebar = double.parse(stdin.readLineSync()!);
+
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  print('\nLuas persegi panjang = ${luas.toStringAsFixed(1)}');
+  print('Keliling persegi panjang = ${keliling.toStringAsFixed(1)}');
+}
